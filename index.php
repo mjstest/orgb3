099184a3c9b7d14ffcc918e6a6ad36ad532a7027
morejust.store <store@morejust.store>
2019-08-18 00:56:07 +0000

    add index.php to 580625b4433e7adde0b54625a869913f branch

---
+++ index.php
@@ -0,0 +1,16 @@
+<Html>
+<Head><Title>Cloud Storage</Title></Head>
+<Body>
+	<p>
+	This cloud storage service is provided by SchwiftyCold.
+	</p>
+	<p>
+	The server is running on google cloud.
+	</p>
+	<p>
+	Cloud is forked from NextCloud.
+	</p>
+	<p>
+	To start your cloud service <a href = "setup-nextcloud.php">click here</a>
+	</p>
+</Body>
